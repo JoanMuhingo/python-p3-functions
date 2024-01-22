a2825da9feb3a33555178722a176667b02f30430
Joan <joanmuhingo@gmail.com>
2024-01-22 21:43:26 +0300

    complete

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print ("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Joan")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+add(10, 9)   
 
 def halve(number):
-    pass
+    return number / 2
+
+halve(60)    
+
+def my_function(param):
+    print("Running my_function")
+    return param + 1
+
+# New code goes here!
+
+my_function_return_value = my_function(1)
+# Running my_function
+# 2
+my_function_return_value
+# 2
